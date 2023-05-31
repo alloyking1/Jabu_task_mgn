0759566ec26b65f6bccfbe8031f0b50ab1e6266d
alloyking1 <alloyking1@gmail.com>
2023-05-31 19:04:32 +0100

    dark mood removed

--- Modules/TaskManager/Resources/views/components/pages/show-task.blade.php
+++ Modules/TaskManager/Resources/views/components/pages/show-task.blade.php
@@ -30,11 +30,11 @@
        
         <input type="checkbox" value="1" class="sr-only peer pt-4" @checked($status) wire:click="updateTaskStatus({{ $id }})">
         
-        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none 
-        peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer
-         dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] 
+        <div class="w-11 h-6 peer-focus:outline-none 
+        peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer
+      bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] 
          after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
-         after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+         after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-blue-600"></div>
         <span class="ml-3 text-md font-semibold text-gray-900">{{ $status? 'done' : 'pending' }}</span>
       </label>
 
